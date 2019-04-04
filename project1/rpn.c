3d2b1572e0403336004dfcfb117c2f2cd8886938
ThotPatrol <daniel.dimitric91@gmail.com>
2019-04-04 23:11:18 +0200

    something something struct node

--- project1/rpn.c
+++ project1/rpn.c
@@ -2,32 +2,18 @@
 
 #define DEBUG(message) printf("Line [%d] - %s\n", __LINE__, message);
 
-typedef struct {
-  int *stack;
-  size_t used;
-  size_t size;
-} dynamicStack;
+typedef struct Node
+{
+        int data;
+        struct Node *next;
+} node;
 
-void initStack(dynamicStack *a, size_t initialSize) {
-  a->stack = (int *)malloc(initialSize * sizeof(int));
-  a->used = 0;
-  a->size = initialSize;
-}
-
-void insertStack(dynamicStack *a, int element) {
-  // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
-  // Therefore a->used can go up to a->size
-  if (a->used == a->size) {
-    a->size *= 2;
-    a->stack = (int *)realloc(a->stack, a->size * sizeof(int));
-  }
-  a->array[a->used++] = element;
-}
-
-void freeArray(dynamicStack *a) {
-  free(a->stack);
-  a->stack = NULL;
-  a->used = a->size = 0;
+void insert(node *n_pointer, int data)
+{
+        while(n_pointer->next != NULL)
+        {
+                pointer = pointer -> next;
+        }
 }
 
 int main(int argc, char *argv[])
