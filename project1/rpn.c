faa41145f32027a3e9d8925411b4b212187ce5c1
ThotPatrol <daniel.dimitric91@gmail.com>
2019-04-06 08:24:07 +0200

    struct node almost done

--- project1/rpn.c
+++ project1/rpn.c
@@ -1,22 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DEBUG(message) printf("Line [%d] - %s\n", __LINE__, message);
 
-typedef struct Node
+typedef struct node node;
+
+struct node
 {
         int data;
-        struct Node *next;
-} node;
+        node *next;
+};
 
-void insert(node *n_pointer, int data)
+void print_list(node *n_ptr)
 {
-        while(n_pointer->next != NULL)
+        node *tmp = n_ptr;
+        printf("List: \n");
+        while(tmp != NULL)
         {
-                pointer = pointer -> next;
+                printf("[%d, %p] -> ", tmp->data, tmp->next);
+                tmp = tmp -> next;
         }
+        printf("\n");
 }
 
-int main(int argc, char *argv[])
+node *new_list(int data)
 {
+        node *first = malloc(sizeof(node));
+        first->next = NULL;
+        first->data = data;
+        return first;
+}
+
+void append(node *n, int data)
+{
+        node *tmp = n;
+        while(tmp->next != NULL){
+                tmp = tmp->next;
+        }
+        tmp->next = malloc(sizeof(node));
+        tmp = tmp->next;
+        tmp->next = NULL;
+        tmp->data = data;
+}
+
+int main()
+{
+        node *list = new_list(10);
+        print_list(list);
+        append(list, 11);
+        print_list(list);
+        append(list, 12);
+        print_list(list);
+        append(list, 13);
         return 0;
 }
