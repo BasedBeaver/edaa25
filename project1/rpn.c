44facdd963901f41e65bb5e2cfa3eb512b93210a
ThotPatrol <daniel.dimitric91@gmail.com>
2019-04-09 13:44:50 +0200

    stack almost done

--- project1/rpn.c
+++ project1/rpn.c
@@ -3,61 +3,91 @@
 
 #define DEBUG(message) printf("Line [%d] - %s\n", __LINE__, message);
 
-typedef struct node node;
+typedef struct stack_node stack_node;
 
-struct node
+struct stack_node
 {
-        int data;
-        node *next;
+        signed int data;
+        stack_node *prev;
 };
 
-void print_list(node *n_ptr)
+void print_list(stack_node *stack_ptr)
 {
-        node *tmp = n_ptr;
+        stack_node *tmp = stack_ptr;
         printf("List: \n");
         while(tmp != NULL)
         {
-                printf("[%d, %p] -> ", tmp->data, tmp->next);
-                tmp = tmp -> next;
+                printf("[%d, %p] -> ", tmp-> data, tmp-> prev);
+                tmp = tmp-> prev;
         }
         printf("\n");
 }
 
-node *new_list(int data)
+stack_node *new_stack(int data)
 {
-        node *first = malloc(sizeof(node));
-        first->next = NULL;
-        first->data = data;
+        stack_node *first = malloc(sizeof(stack_node));
+        first-> prev = NULL;
+        first-> data = data;
         return first;
 }
 
-void append(node *n, int data)
+//Method for single linked list instead of stack
+// void append(stack_node *stack_ptr, int data)
+// {
+//         stack_node *tmp = stack_ptr;
+//         while(tmp-> prev != NULL){
+//                 tmp = tmp-> prev;
+//         }
+//         tmp-> prev = malloc(sizeof(stack_node));
+//         tmp = tmp-> prev;
+//         tmp-> prev = NULL;
+//         tmp-> data = data;
+// }
+
+void push(stack_node **stack_ptr, int data)
 {
-        node *tmp = n;
-        while(tmp->next != NULL){
-                tmp = tmp->next;
+        stack_node *new = malloc(sizeof(stack_node));
+
+        new-> prev = *stack_ptr;
+        new-> data = data;
+
+        *stack_ptr = new;
+}
+
+void pop(stack_node *n_ptr)
+{
+
+}
+
+int operand(int c)
+{
+        int res = 0;
+        if (c == '+' || c == '-' || c == '*' || c == '/') {
+                res = 1;
         }
-        tmp->next = malloc(sizeof(node));
-        tmp = tmp->next;
-        tmp->next = NULL;
-        tmp->data = data;
+        return res;
 }
 
 int main()
 {
+
+        stack_node *stack = new_stack(-1);
+        push(&stack, 10);
+        push(&stack, 11);
+        push(&stack, 12);
+        print_list(stack);
+
         int c, last_c = '\0';
         while ((c = getchar()) != EOF)
         {
-                if (!isspace(c)){
+                if (isdigit(c)) {
                         putchar(c);
+                        printf("\n");
                 }
-                if (!isspace(last_c)) {
-                        putchar('\n');
+                if (operand(c)) {
+                        putchar(c);
+                        printf("\n");
                 }
-                last_c = c;
-        }
-        if (last_c != '\0' && !isspace(last_c)) {
-                putchar('\n');
         }
         return 0;
 }
