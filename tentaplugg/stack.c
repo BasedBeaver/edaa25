4f60d7bc73e499120a1f4f14ef984aac7f11c2c1
ThotPatrol <daniel.dimitric91@gmail.com>
2019-05-28 12:08:31 +0200

    stack with head struckt works perfect

--- tentaplugg/stack.c
+++ tentaplugg/stack.c
@@ -1,19 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct stack_head stack_head;
 typedef struct stack_t stack_t;
-
-struct stack_head {
-	int size;
-	stack_t* stack;
-};
+typedef struct stack_node stack_node;
 
 struct stack_t {
-	int data;
-	stack_t* prev;
+	int size;
+	stack_node* head;
 };
 
+struct stack_node {
+	int data;
+	stack_node* prev;
+};
+
+stack_t* new_stack()
+{
+	stack_t* stack;
+	stack = malloc(sizeof(stack_t));
+
+	if (stack == NULL) {
+		exit(1);
+	}
+
+	stack->size = 0;
+	stack->head = NULL;
+
+	return stack;
+}
+
+static stack_node* new_node(int data)
+{
+	stack_node* node;
+	node = malloc(sizeof(stack_node));
+
+	if (node == NULL) {
+		exit(1);
+	}
+
+	node->data = data;
+	node->prev = NULL;
+
+	return node;
+}
+
+void push(stack_t* stack, int data)
+{
+	stack_node* node = new_node(data);
+
+	stack_node* tmp = stack->head;
+	stack->head = node;
+	node->prev = tmp;
+
+	stack->size++;
+}
+
+int pop(stack_t* stack)
+{
+	stack_node* tmp = stack->head;
+
+	int data = tmp->data;
+	stack->head = stack->head->prev;
+
+	free(tmp);
+
+	stack->size--;
+
+	return data;
+}
+
+void free_stack(stack_t** stack)
+{
+	stack_t* p = *stack;
+
+	while(p->head != NULL) {
+		pop(p);
+	}
+	free(p);
+	*stack = NULL;
+}
+
+void print_list(stack_t* stack)
+{
+	 stack_node* tmp = stack->head;
+	 printf("List: \n");
+	 while(tmp != NULL) {
+		 printf("[%d, %p] -> ", tmp->data, tmp->prev);
+		 tmp = tmp->prev;
+	 }
+	 printf("\n");
+}
 
 int main()
 {
@@ -21,12 +97,19 @@ int main()
 	int a;
 
 	stack = new_stack();
-
+	//print_list(stack);
 	push(stack, 2014);
+	//print_list(stack);
+	//printf("stack size = %d and head->data = %d\n", stack->size, stack->head->data); This doesn't work for some reason
 	a = pop(stack);
+	//print_list(stack);
 	push(stack, 10);
+	//print_list(stack);
 	push(stack, 30);
+	//print_list(stack);
 	free_stack(&stack);
+
+	printf("res = %d\n", stack == NULL ? 0 : 1);
 
 	return stack == NULL ? 0 : 1;
 }
