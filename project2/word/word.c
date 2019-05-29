ecb34f0281037fb28732c9e5944137fde0036738
ThotPatrol <daniel.dimitric91@gmail.com>
2019-05-29 09:40:16 +0200

    finally stareted with word

--- project2/word/word.c
+++ project2/word/word.c
@@ -5,7 +5,27 @@ int main()
 {
 	int c;
 	while((c = getchar()) != EOF) {
-		printf("%c\n", (char)(c));
+
+		int length = 0;
+		int size = 10;
+		char* char_ptr;
+		char* first;
+		list = malloc(sizeof(char) * 10);
+		if (char_ptr == NULL) {
+			exit(1);
+		}
+
+		if (isalpha(c)) {
+			if (length >= size) {
+				list = realloc(list, size*2);
+				if (list == NULL) {
+					exit(1);
+				} else {
+					size = size*2;
+				}
+			}
+			printf("%c\n", (char)(c));
+		}
 	}
 	return 0;
 }
