ff88457282ac7746fce37fd27259e97290bee41a
ThotPatrol <daniel.dimitric91@gmail.com>
2019-06-01 14:21:22 +0200

    stared some more

--- project3/poly/poly.c
+++ project3/poly/poly.c
@@ -3,11 +3,30 @@
 
 #include "poly.h"
 
-#define size 10;
+#define MAX_SIZE 100;
 
 struct poly_t {
-	int coef[size];
-	int exp[size];
+	int coef[MAX_SIZE];
+	int exp[MAX_SIZE];
+	int size;
 };
 
-/* Make complete! */
+poly_t*	new_poly_from_string(const char* c)
+{
+
+}
+
+void free_poly(poly_t* p)
+{
+	free(p);
+}
+
+poly_t*	mul(poly_t* p1, poly_t* p2)
+{
+
+}
+
+void print_poly(poly_t* p)
+{
+
+}
