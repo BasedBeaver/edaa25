483239de39743bbdc1523a717351dbb54156dbf8
ThotPatrol <daniel.dimitric91@gmail.com>
2019-06-01 15:17:28 +0200

    break

--- project3/poly/poly.c
+++ project3/poly/poly.c
@@ -1,19 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "poly.h"
 
-#define MAX_SIZE 100;
+#define MAX_SIZE 100
 
 struct poly_t {
-	int coef[MAX_SIZE];
+	int coeff[MAX_SIZE];
 	int exp[MAX_SIZE];
 	int size;
 };
 
+struct poly_t* new_poly()
+{
+	struct poly_t* p = malloc(sizeof(struct poly_t));
+	memset(p->coeff, 0, MAX_SIZE * sizeof(int));
+	memset(p->exp, 0, MAX_SIZE * sizeof(int));
+	p->size = 0;
+	return p;
+}
+
 poly_t*	new_poly_from_string(const char* c)
 {
+	struct poly_t* poly = new_poly();
+	printf("TRYING TO PRINT SOME%s\n", c);
 
+	for (i = 0; i < strlen(str); i++) {
+
+
+
+	}
+
+
+	return poly;
 }
 
 void free_poly(poly_t* p)
@@ -23,10 +43,10 @@ void free_poly(poly_t* p)
 
 poly_t*	mul(poly_t* p1, poly_t* p2)
 {
-
+	return NULL;
 }
 
 void print_poly(poly_t* p)
 {
-
+	printf("printpoly\n");
 }
