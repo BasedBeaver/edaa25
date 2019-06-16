fb263f202bf62289e6894d76381202f23b2ed0a2
ThotPatrol <daniel.dimitric91@gmail.com>
2019-06-16 21:40:00 +0200

    something something

--- data-structures/double_linked_list.c
+++ data-structures/double_linked_list.c
@@ -5,6 +5,7 @@ typedef struct list_t list_t;
 typedef struct node_t node_t;
 
 struct list_t {
+	int size;
 	node_t* first;
 	node_t* last;
 };
@@ -23,10 +24,52 @@ list_t* new_list()
 		exit(1);
 	}
 
+	list->size = 0;
 	list->first = NULL;
 	list->last = NULL;
 
 	return list;
+}
+
+node_t* new_node(void* data)
+{
+	node_t* node = malloc(sizeof(node_t));
+
+	if (node == NULL) {
+		exit(1);
+	}
+
+	node->data = data;
+	node->next = NULL;
+	node->prev = NULL;
+
+	return node;
+}
+
+void insert_first(list_t* list, void* data)
+{
+	node_t* n = new_node(data);
+
+	if (list->size == 0) {
+		list->last = n;
+	}
+	else {
+		n->next = list->first;
+	}
+	list->first->prev = n;
+	list->first = n;
+}
+
+
+void print_list(list_t* list)
+{
+	 stack_node* tmp = list->head;
+	 printf("List: \n");
+	 while(tmp != NULL) {
+		 printf("[%d, %p] -> ", tmp->data, tmp->prev);
+		 tmp = tmp->prev;
+	 }
+	 printf("\n");
 }
 
 
